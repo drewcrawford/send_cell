2a5a0f155e79f3a2589d259b1574186c8a8a19a9
Drew Crawford <drew@sealedabstract.com>
2025-08-12 17:14:02 -0500

    temporary workaround to diagnose CI issue

--- src/unsafe_send_cell.rs
+++ src/unsafe_send_cell.rs
@@ -549,6 +549,7 @@ mod tests {
     // Helper function to verify a type implements Send
     fn assert_send<T: Send>(_: &T) {}
 
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
     #[test]
     fn test_non_send_future_to_send_future() {
         // Create a non-Send future
@@ -570,6 +571,7 @@ mod tests {
         // For example, we could spawn it on a thread pool (though we won't actually do that here)
     }
 
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
     #[test]
     fn test_future_functionality_preserved() {
         use std::task::{RawWaker, RawWakerVTable, Waker};
